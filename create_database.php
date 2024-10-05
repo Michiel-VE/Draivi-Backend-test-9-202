b3b2f7c72e6c6ee6484518dc56d7410899dc5bc0
Michiel_VE <r0794174@student.thomasmore.be>
2024-10-05 15:33:37 +0300

    Part 1 completed, data can be added, update time when price is updated

--- create_database.php
+++ create_database.php
@@ -1,13 +1,19 @@
 <?php
 
+require_once 'readFile.php';
+require_once 'getCurrencyValue.php';
+
 $env = parse_ini_file('.env');
-$priceListUrl= "https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xlsx";
+$priceListUrl = "https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xlsx";
 
 $serverName = $env['SERVER_NAME'];
 $userName = $env['USER_NAME'];
 $password = $env['PASSWORD'];
+$api = $env['API'];
 
+$currencyUrl = "http://apilayer.net/api/live?access_key=$api&currencies=GBP&source=EUR&format=1";
 
+$gbpvalue = getGBPValue($currencyUrl);
 
 $conn = new mysqli($serverName, $userName, $password);
 if ($conn->connect_error) {
@@ -21,7 +27,7 @@ $create_db = "CREATE DATABASE IF NOT EXISTS alkopricelist";
 if ($conn->query($create_db) === true) {
     echo "DB created\n";
 } else {
-    echo "Error creating" . ' '. $conn->error;
+    echo "Error creating" . ' ' . $conn->error . '\n';
 }
 
 
@@ -38,21 +44,39 @@ $table_query = "CREATE TABLE IF NOT EXISTS product(
                 orderAmount int DEFAULT 0 
                 )";
 
-if ($conn->query($table_query) === true){
-    echo "table created";
+if ($conn->query($table_query) === true) {
+    echo "table created\n";
 } else {
-    echo "Error creating table" . ' ' . $conn->error;
+    echo "Error creating table" . ' ' . $conn->error . '\n';
 }
 
 
 try {
     $excelData = readExcelFromUrl($priceListUrl);
-    echo $excelData;
 
-//    // Output the data
-//    foreach ($excelData as $row) {
-//        echo implode(", ", $row) . "\n"; // Print each row
-//    }
+    $insertquery = $conn->prepare("INSERT INTO product (number, name, bottleSize, price, priceGBP) VALUES (?,?,?,?,?)
+                                         ON DUPLICATE KEY UPDATE 
+                                             name = VALUES(name), 
+                                             bottleSize = VALUES(bottleSize), 
+                                             price = VALUES(price), 
+                                             priceGBP = VALUES(priceGBP) ");
+
+
+    for ($i = 4; $i < count($excelData); $i++) {
+        $row = $excelData[$i];
+
+        $number = $row[0];
+        $name = $row[1];
+        $bottleSize = $row[3];
+        $price = $row[4];
+        $priceGBP = (float)$price * (float)$gbpvalue;
+
+        $insertquery->bind_param('issdd', $number, $name, $bottleSize, $price, $priceGBP);
+
+        $insertquery->execute();
+    }
+
+    $insertquery->close();
 } catch (Exception $e) {
     echo 'Error: ', $e->getMessage();
 }
---
+++ getCurrencyValue.php
@@ -0,0 +1,21 @@
+<?php
+
+function getGBPValue($url) {
+    $req = curl_init();
+
+    curl_setopt($req, CURLOPT_URL, $url);
+    //enable to save the value
+    curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($req);
+
+
+    if ($response === false) {
+        echo 'Error: ' . curl_error($req);
+    } else {
+        $data = json_decode($response, true);
+        return $data['quotes']['EURGBP'];
+    }
+
+    curl_close($req);
+}
